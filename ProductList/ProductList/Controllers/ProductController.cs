98afc8943bc1b20d372f96f77018986c02861f91
sshind31 <101928160+sshind31@users.noreply.github.com>
2023-10-17 08:09:33 +0530

    Add files via upload

---
+++ ProductList/ProductList/Controllers/ProductController.cs
@@ -0,0 +1,101 @@
+﻿using ProductList.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProductList.Controllers
+{
+    public class ProductController : Controller
+    {
+        Product product = new Product();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> List = new List<Product>();
+            try
+            {
+                List = product.GetListOfProduct();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            return View(List);
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(Product prod)
+        {
+            try
+            {
+                bool check = product.InsertToDB(prod);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ProductList/ProductList/Models/Product.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace ProductList.Models
+{
+    public class Product
+    {
+        [Required]
+        public int ProductId { get; set; }
+
+        [DisplayName("Product Name")]
+        public string ProductName { get; set; }
+
+        [Required]
+        public decimal Rate { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        private string conString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=JKJuly2022;Integrated Security=True";
+
+        //InsertProductToDB
+        public bool InsertToDB(Product prod)
+        {
+            int id = 0;
+
+            SqlConnection connect = new SqlConnection(conString);
+            try 
+            {
+                SqlCommand cmd = new SqlCommand("InsertProductToDB",connect);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@ProductId", prod.ProductId);
+                cmd.Parameters.AddWithValue("@ProductName", prod.ProductName);
+                cmd.Parameters.AddWithValue("@Rate", prod.Rate);
+                cmd.Parameters.AddWithValue("@Description", prod.Description);
+                cmd.Parameters.AddWithValue("@Category", prod.Category);
+                connect.Open();
+                id =cmd.ExecuteNonQuery();
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                connect.Close();
+            }
+
+            return id > 0;
+        }
+
+        //AllProductList
+        public List<Product> GetListOfProduct()
+        {
+            List<Product> ListOfProduct = new List<Product>();
+
+            SqlConnection con = new SqlConnection(conString);
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("AllProductList", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataReader dr = cmd.ExecuteReader();
+                while(dr.Read())
+                {
+                    ListOfProduct.Add(new Product
+                    {
+                        ProductId=Convert.ToInt32(dr["ProductId"]),
+                        ProductName=(dr["ProductName"]).ToString(),
+                        Rate = Convert.ToDecimal(dr["ProductId"]),
+                        Description= (dr["Description"]).ToString(),
+                        Category= (dr["Category"]).ToString()
+                    });
+                }
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                con.Close();
+            }
+
+            return ListOfProduct;
+        }
+    }
+}
